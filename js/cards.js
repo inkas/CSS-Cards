f43a061a6269d141e3f7bdade5d2c2f2ae45538e
inkas91 <inkas91@gmail.com>
2016-01-04 18:43:08 +0200

    Upload

---
+++ js/cards.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$(".card-container .sign, .card-container .sign-rotate").fitText(0.40);
+	$(".card-container .symbol-small").fitText(0.55);
+	$('.card-container .symbol').fitText(0.28);
+	$("div[class$='-clubs'] .symbol").fitText(0.30);
+	$('.card-A-diamonds .symbol, .card-A-hearts .symbol, .card-A-spades .symbol, .card-A-clubs .symbol,card-A-diamonds .symbol, .card-A-hearts .symbol, .card-A-spades .symbol, .card-A-clubs .symbol, .card-J-diamonds .symbol, .card-J-hearts .symbol, .card-J-spades .symbol, .card-J-clubs .symbol,card-J-diamonds .symbol, .card-J-hearts .symbol, .card-J-spades .symbol, .card-J-clubs .symbol, .card-K-diamonds .symbol, .card-K-hearts .symbol, .card-K-spades .symbol, .card-K-clubs .symbol,card-K-diamonds .symbol, .card-K-hearts .symbol, .card-K-spades .symbol, .card-K-clubs .symbol, .card-Q-diamonds .symbol, .card-Q-hearts .symbol, .card-Q-spades .symbol, .card-Q-clubs .symbol,card-Q-diamonds .symbol, .card-Q-hearts .symbol, .card-Q-spades .symbol, .card-Q-clubs .symbol').fitText(0.12);
+});
